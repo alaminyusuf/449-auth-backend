67b05139778fb9a0ec64b167219e867b620c338c
Al-amin Yusuf <alaminyusuf131@gmail.com>
2025-10-20 09:54:40 +0100

    updated routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,22 +4,39 @@ const router = express.Router()
 const authController = require('./auth')
 const passwordController = require('../controllers/password')
 const otpController = require('../controllers/opt')
-const protected = require('../middleware/auth')
+const { protect } = require('../middleware/auth')
 const staffController = require('../controllers/staff')
+const addminController = require('../controllers/admin')
 
 router.post('/auth/register', authController.register)
 router.post('/auth/login', authController.login)
-router.post('/send-otp', protected, otpController.sendOTPEmail)
-router.post('/verify-otp', protected, otpController.verifyOTP)
+router.post('/send-otp', { protect }, otpController.sendOTPEmail)
+router.post('/verify-otp', { protect }, otpController.verifyOTP)
 router.post('/forgot-password', passwordController.forgotPassword)
-router.post('/reset-password', protected, passwordController.resetPassword)
-router.post('/staff/add-income', protected, staffController.addIncome)
-router.post('/staff/add-expense', protected, staffController.addExpense)
-router.get('/staff/get-summary', protected, staffController.getSummary)
+router.post(
+	'/reset-password',
+	{ protect },
+	passwordController.resetPassword
+)
+router.post('/staff/add-income', { protect }, staffController.addIncome)
+router.post('/staff/add-expense', { protect }, staffController.addExpense)
+router.get('/staff/get-summary', { protect }, staffController.getSummary)
 router.get(
 	'/staff/get-transactions',
-	protected,
+	protect,
 	staffController.getTransactions
+)
+router.get(
+	'/admin/view-pay-history',
+	protect,
+	admin,
+	addminController.viewPayHistory
+)
+router.post(
+	'/admin/pay-salaries',
+	protect,
+	admin,
+	addminController.paySalaries
 )
 
 module.exports = router
